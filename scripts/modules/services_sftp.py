c7fe39c69bb510315ea44d7d1444a2c0f7628c8f
Ernesto Quito Gonzales <ernesto.quito@externos-cl.cencosud.com>
2023-02-07 20:00:18 -0500

    feat: Se agrega el modulo para sftp y se actualiza el archivo requirements

---
+++ scripts/modules/services_sftp.py
@@ -0,0 +1,34 @@
+import pysftp
+import os
+
+class ServicesSFTP(object):
+    host = None
+    username = None
+    password = None
+    cnopts = None
+
+    def __init__(self, host, user, password):
+        self.host = host
+        self.username = user
+        self.password = password
+
+    def set_cnopts(self, path_file: str):
+        self.cnopts = pysftp.CnOpts(knownhosts=path_file)
+
+    def put_files(self, local_path_file, remote_path_file):
+        with pysftp.Connection(
+            host=self.host,
+            username=self.username,
+            password=self.password,
+            cnopts=self.cnopts
+        ) as sfpt:
+            sfpt.put(localpath=local_path_file, remotepath=remote_path_file)
+
+    def get_files(self, local_path_file, remote_path_file):
+        with pysftp.Connection(
+            host=self.host,
+            username=self.username,
+            password=self.password,
+            cnopts=self.cnopts
+        ) as sfpt:
+            sfpt.get(remotepath=remote_path_file, localpath=local_path_file)
