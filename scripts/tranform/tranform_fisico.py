855b077106d12499accb665f935dfe8a351fcdb4
Ernesto Quito Gonzales <ernesto.quito@externos-cl.cencosud.com>
2023-02-08 19:53:54 -0500

    feat: Se agrega carpetas

---
+++ scripts/tranform/tranform_fisico.py
@@ -0,0 +1,13 @@
+def tranform_data_fisico(data):
+    # TODO
+    data['fecha_carga']= datetime.now()
+    data2 = data[""] == data[]
+    
+    retun data_clean
+
+ef tranform_data_fisico_actual(data):
+    # TODO
+    data['fecha_carga']= datetime.now()
+    data2 = data[""] == data[]
+    
+    retun data_clean
